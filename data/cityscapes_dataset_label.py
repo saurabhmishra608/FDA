d33d51684d562a93859b6ae22d9471a0e5a6e21f
saurabhmishra608 <saurabhmishra608@gmail.com>
2020-12-18 11:45:32 +0530

    Update cityscapes_dataset_label.py

--- data/cityscapes_dataset_label.py
+++ data/cityscapes_dataset_label.py
@@ -29,7 +29,7 @@ class cityscapesDataSetLabel(data.Dataset):
 
     def __getitem__(self, index):
         name = self.img_ids[index]   # aachen/aachen_000000_000019_leftImg8bit.png
-        image = Image.open(osp.join(   self.root, "leftImg8bit/%s/%s" % (self.set, name)   )).convert('RGB')
+        image = Image.open(osp.join(   "../cityscapesScripts/", "leftImg8bit/%s/%s" % (self.set, name)   )).convert('RGB')
         lbname = name.replace("leftImg8bit", "gtFine_labelIds")
         label = Image.open(osp.join(   self.root, "gtFine/%s/%s" % (self.set, lbname)   ))
 
